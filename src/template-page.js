42b3c781ec388793984bb8fa71bb69d911c01fd3
1Clide1 <91157843+1Clide1@users.noreply.github.com>
2021-12-15 23:31:27 -0500

    I fixed the template added some icons and fixed the classes
    fixed all the classes and added some icons for the job titles for style points

--- src/template-page.js
+++ src/template-page.js
@@ -4,15 +4,15 @@ const generateTeam= team =>{
     const generateManager = manager =>{
         return `
         <div class="employee-card">
-    <div class="header">
-            <h2 class="title">${manager.getName()}</h2>
-            <h3 class="title">${manager.getRole()}</h3>
+    <div class="card-header">
+            <h2 class="name-title">${manager.getName()}</h2>
+            <h3 class="job-title"><i class="lni lni-briefcase"></i> ${manager.getRole()}</h3>
         </div>
-        <div class="body">
+        <div class="card-body">
             <ul class="emp-list">
-                <li class="list item">ID:${manager.getId()}</li>
-                <li class="list item">Email:<a href = "mailto: ${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list item">Office Number:${manager.getOfficeNumber()}</li>
+                <li class="list-item">ID:${manager.getId()}</li>
+                <li class="list-item">Email:<a href = "mailto: ${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-item">Office Number:${manager.getOfficeNumber()}</li>
             </ul>
         </div>
     </div>
@@ -22,15 +22,15 @@ const generateTeam= team =>{
     const generateEngineer = engineer =>{
         return `
         <div class="employee-card">
-    <div class="header">
-            <h2 class="title">${engineer.getName()}</h2>
-            <h3 class="title">${engineer.getRole()}</h3>
+    <div class="card-header">
+            <h2 class="name-title">${engineer.getName()}</h2>
+            <h3 class="job-title"><i class="lni lni-invention"></i> ${engineer.getRole()}</h3>
         </div>
-        <div class="body">
+        <div class="card-body">
             <ul class="emp-list">
-                <li class="list item">ID:${engineer.getId()}</li>
-                <li class="list item">Email:<a href = "mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                <li class="list item">Github:<a href= "https://github.com/${engineer.getGithub()}" target"_blank">${engineer.getGithub()}</a></li>
+                <li class="list-item">ID:${engineer.getId()}</li>
+                <li class="list-item">Email:<a href = "mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-item">Github:<a href= "https://github.com/${engineer.getGithub()}" target"_blank">${engineer.getGithub()}</a></li>
             </ul>
         </div>
     </div>
@@ -40,15 +40,15 @@ const generateTeam= team =>{
     const generateIntern = intern =>{
         return `
         <div class="employee-card">
-    <div class="header">
-            <h2 class="title"> ${intern.getName()}</h2>
-            <h3 class="title">${intern.getRole()}</h3>
+    <div class="card-header">
+            <h2 class="name-title"> ${intern.getName()}</h2>
+            <h3 class="job-title"><i class="lni lni-graduation"></i> ${intern.getRole()}</h3>
         </div>
-        <div class="body">
+        <div class="card-body">
             <ul class="emp-list">
-                <li class="list item">ID:${intern.getId()}</li>
-                <li class="list item">Email:<a href = "mailto: ${intern.getEmail()}">${intern.getEmail()}</a></li>
-                <li class="list item">School:${intern.getSchool()}</li>
+                <li class="list-item">ID:${intern.getId()}</li>
+                <li class="list-item">Email:<a href = "mailto: ${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-item">School:${intern.getSchool()}</li>
             </ul>
         </div>
     </div>
@@ -75,10 +75,12 @@ const generateTeam= team =>{
         .map(intern => generateIntern(intern))
         .join("")
     );
+    // this basically just returns everything we just joined together in this function
     return html.join("")
 };
 
 // export function to generate the entire html page
+// also defining what team is in this case it is the first half of the html that we are adding into with the functions above
 module.exports= team =>{
     return `
     <!DOCTYPE html>
@@ -89,20 +91,17 @@ module.exports= team =>{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Team B)</title>
     <link rel="stylesheet" href="./dist/stylesheet.css">
+    <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
 </head>
 <body>
 <div class="team-header-container">
     <div class="team-header">
-        <h1 class="team title">My Team</h1>
+        <h1>My Team</h1>
     </div>
 </div>
-    <div class="container">
-        <div class="flex-row">
-            <div class"team-container">
+            <div class="team-container">
                 ${generateTeam(team)}
             </div>
-        </div>
-    </div>
 </body>
 </html>
 `;
